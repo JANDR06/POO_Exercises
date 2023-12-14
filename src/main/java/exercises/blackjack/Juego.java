6579289769828c4d4f1ebdca6721b0bb4ccf0b37
ajorgep <alejorpas2@alu.edu.gva.es>
2023-12-14 18:36:23 +0100

    Commit

--- src/main/java/exercises/blackjack/Juego.java
+++ src/main/java/exercises/blackjack/Juego.java
@@ -2,10 +2,43 @@ package exercises.blackjack;
 
 public class Juego {
 
-    public static void main(String[] args) {
+    private Baraja baraja;
+    private Jugador[] jugadores;
+    private Jugador jugadorPC;
 
-        Baraja b1 = new Baraja();
-        System.out.println(b1);
+    public Juego(Jugador[] jugadores) {
+        this.jugadores = jugadores;
+        baraja = new Baraja();
+        jugadorPC = new Jugador("BANCA");
+    }
+
+    public void start() {
+        // repartir las 2 cartas
+
+        // jugara cada jugador
+        for (Jugador jugador : jugadores) {
+            juegaJugador(jugador);
+        }
+
+        juegaPC(jugadorPC);
+
+        mostrarGanador();
+    }
+
+    public void juegaJugador(Jugador jugador) {
+
+    }
+
+    private int maxPuntuacionJugadores() {
+        return 0;
+    }
+
+    public void juegaPC(Jugador jugador) {
+
+
+    }
+
+    public void mostrarGanador() {
 
     }
 }
